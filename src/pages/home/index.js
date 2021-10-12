d70c192e064493c3590d254be8da289b192ae535
Emerson Vieira <emevieira@gmail.com>
2021-10-12 17:31:59 -0400

    Inicio da estruturacao da apresentacao do filme principal

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <>
             <header>
                 <div className="container">
-                    <h2 className="logo">NETFLIX</h2>
+                    <h2 className="logo">ULTRAFIX</h2>
                     <nav>
                         <a href="http://localhost:3000/">Início</a>
                         <a href="http://localhost:3000/">Séries</a>
@@ -16,6 +16,31 @@ export default function Home() {
                     </nav>
                 </div>
             </header>
+
+            <main>
+                <div className="filme-principal">
+                    <div className="container">
+                        <h3 className="titulo">VENOM</h3>
+                        <p className="descricao">
+                            O jornalista Eddie Brock desenvolve
+                            força e poder sobre-humanos quando seu
+                            corpo se funde com o alienígena Venom.
+                            Dominado pela raiva, Venom tenta controlar
+                            as novas e perigosas habilidades de Eddie
+                        </p>
+
+                        <div className="botoes">
+                            <button className="btn">
+                                ASSISTIR AGORA
+                            </button>
+                            <button className="btn">
+                                MAIS INFORMAÇÕES
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </main>
         </>
     );
 }
