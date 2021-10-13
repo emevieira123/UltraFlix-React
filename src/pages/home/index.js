8bf22812642e271758030d203347d0efb8580ed4
Emerson Vieira <emevieira@gmail.com>
2021-10-12 21:26:04 -0400

    Estilizando a descricao e os botoes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 // import capa from "../../assets/capa.jpeg";
 import './style.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+
 export default function Home() {
     return (
         <>
@@ -31,9 +34,11 @@ export default function Home() {
 
                         <div className="botoes">
                             <button className="btn">
+                                <FontAwesomeIcon className="btnIcon" icon={faPlay} />
                                 ASSISTIR AGORA
                             </button>
                             <button className="btn">
+                                <FontAwesomeIcon className="btnIcon" icon={faInfoCircle} />
                                 MAIS INFORMAÇÕES
                             </button>
                         </div>
